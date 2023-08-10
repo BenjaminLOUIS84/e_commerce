6770a9a4fb9089c2beafbeefe449e5c32c95af12
Benjamin Louis <louisbenetval@orange.fr>
2023-08-10 15:29:15 +0200

    MAJ

--- e_commerce/src/Entity/User.php
+++ e_commerce/src/Entity/User.php
@@ -9,7 +9,9 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Un compte est déjà associé à cette adresse mail')] // Spécifie que le mail est unique (généré auto)
+#[UniqueEntity(fields: ['pseudo'], message: 'Un compte est déjà associé à ce pseudo')] // Spécifie que le pseudo est unique (à ajouter)
+
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -17,7 +19,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 180, unique: true)]
+    #[ORM\Column(length: 180, unique: true)] // Spécifie que le mail est unique (généré auto)
     private ?string $email = null;
 
     #[ORM\Column]
@@ -32,7 +34,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'boolean')]
     private $isVerified = false;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)] // Spécifie que le pseudo est unique (à ajouter)
     private ?string $Pseudo = null;
 
     public function getId(): ?int
