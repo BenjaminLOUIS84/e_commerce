2df5317720a3118ca39f3e5ebbcd21e0c0ca2fcd
Benjamin Louis <louisbenetval@orange.fr>
2023-08-10 02:58:37 +0200

    MAJ

--- e_commerce/src/Form/RegistrationFormType.php
+++ e_commerce/src/Form/RegistrationFormType.php
@@ -5,10 +5,13 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
@@ -18,12 +21,13 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email',EmailType::class)
+            ->add('pseudo', TextType::class)
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Accepter les CGV pour vous inscrire',
                     ]),
                 ],
             ])
@@ -31,11 +35,29 @@ class RegistrationFormType extends AbstractType
                 
                 'mapped' => false,
                 'type' => PasswordType::class,
-                'invalid_message' => 'The password fields must match.',
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
                 'options' => ['attr' => ['class' => 'password-field']],
                 'required' => true,
-                'first_options'  => ['label' => 'Password'],
-                'second_options' => ['label' => 'Repeat Password'],
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
+            
+                'constraints' => [                                                      
+                    new Length([                                // Pour faire en sorte que le password contienne minimum 12 caractères
+                        
+                        'min' => 12,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins 12 caractères',
+                        
+                        'max' => 4096                           // Nombre maximum de caractères autorisé par Symfony
+                    ]),
+                    
+                    new Regex([                                 // Pour faire en sorte que le password contienne minimum une majuscule,...
+                        
+                        'pattern' => "/^\S*(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=\S*[\W])[a-zA-Z\d]{12,}\S*$/",
+                        'message' => 'Minimum 12 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial',
+                    ]),
+
+                ]
+
             ])
         ;
     }
