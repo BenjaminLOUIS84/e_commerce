71e1195ac80611d3408a7480b191bb5eedf68059
Benjamin Louis <louisbenetval@orange.fr>
2023-08-11 12:07:00 +0200

    MAJ

--- e_commerce/src/Controller/FormatController.php
+++ e_commerce/src/Controller/FormatController.php
@@ -12,7 +12,7 @@ class FormatController extends AbstractController
     #[Route('/format', name: 'app_format')]                                 // Route représentant l'URL '/format' pour la redirection et le name: sert pour la navigation
     public function index(FormatRepository $formatRepository): Response     // Pour afficher la liste des collections insérer dans la fonction index() formatRepository $formatRepository        
     {                                                                       // Importer la classe FormatRepository avec un click droit 
-        $formats = $formatRepository->findBy([],["intitule" => "ASC"]);     // Pour récupérer la liste des formats classées par ordre alphabéthique selon l'intitule
+        $formats = $formatRepository->findBy([],["type" => "ASC"]);         // Pour récupérer la liste des formats classées par ordre alphabéthique selon l'intitule
 
         return $this->render('format/index.html.twig', [                    // render() Permet de faire le lien entre le controller et la view
             'formats' => $formats                                           // Pour passer la variable $formats en argument 'formats'
