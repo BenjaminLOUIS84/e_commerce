63e11a8264193ec571882db4ffd23417dd35839b
Benjamin Louis <louisbenetval@orange.fr>
2023-08-15 16:02:21 +0200

    MAJ

--- e_commerce/src/Form/FormatType.php
+++ e_commerce/src/Form/FormatType.php
@@ -2,7 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\Livre;
+// use App\Entity\Livre;
 use App\Entity\Format;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,11 +18,11 @@ class FormatType extends AbstractType
         $builder
         ->add('type', TextType::class)          // Définir les types de champs et importer les classes
         
-        ->add('Livre', EntityType::class, [     // Particularité ici EntityType à besoin d'un tableau d'arguments pour fonctionner
-        'mapped' => false,                      // Pour permettre l'affichage de ce champs dans le formulaire mettre le mappage en false
-        'class' => Livre::class, 
-        'attr' => ['class' => 'form-control'],
-        'choice_label' => 'titre'])
+        // ->add('Livre', EntityType::class, [     // Particularité ici EntityType à besoin d'un tableau d'arguments pour fonctionner
+        // 'mapped' => false,                      // Pour permettre l'affichage de ce champs dans le formulaire mettre le mappage en false
+        // 'class' => Livre::class, 
+        // 'attr' => ['class' => 'form-control'],
+        // 'choice_label' => 'titre'])
 
         ->add('valider', SubmitType::class, [   // Ajouter directement le bouton submit ici
         'attr' =>['class' => 'btn btn-dark']])  // Ajouter après class ['attr' =>['class' =>'btn btn-dark']] Pour améliorer le bouton   
