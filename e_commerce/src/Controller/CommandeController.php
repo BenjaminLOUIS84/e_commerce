98ade684c329dbe5785ea557098d4af3a1fc197f
Benjamin Louis <louisbenetval@orange.fr>
2023-08-19 23:07:08 +0200

    MAJ

--- e_commerce/src/Controller/CommandeController.php
+++ e_commerce/src/Controller/CommandeController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Livre;
+use App\Entity\Commande;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\CommandeRepository;
-// use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,18 +25,80 @@ class CommandeController extends AbstractController
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // FONCTION FORMULAIRE POUR AJOUTER UN LIVRE DANS LA COMMANDE
+    // FONCTION POUR SUPPRIMER UNE COMMANDE
+
+    #[Route('/commande/{id}/delete', name: 'delete_commande')]          // Reprendre la route en ajoutant /{id}/delete' à l'URL et en changeant le nom du name
+
+    public function delete(Commande $commande, EntityManagerInterface $entityManager): Response   
+
+    {                                                                   // Créer une fonction delete() dans le controller pour supprimer une commande            
+        $entityManager->remove($commande);                              // Supprime une commande
+        $entityManager->flush();                                        // Exécute l'action DANS LA BDD
+
+        $this->addFlash(                                                // Envoyer une notification
+            'success',
+            'Commande supprimée avec succès!'
+        );
+
+        return $this->redirectToRoute('app_commande');                     // Rediriger vers la liste des collections
+       
+    }
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // FONCTION FORMULAIRE POUR AJOUTER et EDITER DES COMMANDES
+
+    #[Route('/commande/new', name: 'new_commande')]                   // Reprendre la route en ajoutant /new à l'URL et en changeant le nom du name
+    #[Route('/commande/{id}/edit', name: 'edit_commande')]            // Reprendre la route en ajoutant /{id}/edit à l'URL et en changeant le nom du name
+
+    public function new_edit(Commande $commande  = null, Request $request, EntityManagerInterface $entityManager): Response   
+    
+    {
+        if(!$commande){                                               // S'il n'ya pas de commande à modifier alors en créer une nouvelle
+            $commande = new Commande();                               // Après avoir importé la classe Request Déclarer une nouvelle commande
+        }
+
+        $form = $this->createForm(CommandeType :: class, $commande);  // Créer un nouveau formulaire avec la méthode createForm() et importer le classe CommandeType
+
+        //////////////////////////////////////////////////////////////////////////
+        //                                                      GERER LE TRAITEMENT EN BDD
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {            // Si le formulaire soumis est valide alors
+            
+            $commande = $form->getData();                          // Récupérer les informations de la nouvelle commande 
+            //prepare PDO
+            $entityManager->persist($commande);                    // Dire à Doctrine que je veux sauvegarder la nouvelle commande           
+            //execute PDO
+            $entityManager->flush();                               // Mettre la nouvelle collection dans la BDD
+
+            $this->addFlash(                                       // Envoyer une notification
+                'success',
+                'Opération réalisée avec succès!'
+            );
+
+            return $this->redirectToRoute('app_commande');         // Rediriger vers la liste des collections
+        }
+
+        //////////////////////////////////////////////////////////////////////////
+
+
+        return $this->render('commande/new.html.twig', [           // Pour faire le lien entre le controller et la vue new.html.twig (il faut donc la créer dans le dossier commande)
+            'formAddCommande' => $form,
+            'edit' => $commande->getId()
+        ]);
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // FONCTION PANIER POUR AJOUTER UN LIVRE DANS LA COMMANDE
 
     #[Route('/livre/{id}/addLivre', name: 'add_livre')]             // Reprendre la route en ajoutant /{id}/edit à l'URL et en changeant le nom du name
 
-    public function addLivre(Livre $livre, 
-    //Request $request, 
-    EntityManagerInterface $entityManager): Response                // Créer une fonction addLivre() dans le controller pour permettre l'ajout d'un livre
+    public function addLivre(Livre $livre, EntityManagerInterface $entityManager): Response                // Créer une fonction addLivre() dans le controller pour permettre l'ajout d'un livre
 
     {
         ////////////////////////////////////////////////////////////GERER LE TRAITEMENT EN BDD
         
-        // $commande->handleRequest($request);
+        // $livre = new Livre();
         
         // $livre->getData();                          // Récupérer les informations du livre 
         //prepare PDO
