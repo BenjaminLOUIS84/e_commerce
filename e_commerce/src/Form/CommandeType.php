755afa2760c2c59ac64a51fe7b892468f4298b88
Benjamin Louis <louisbenetval@orange.fr>
2023-08-19 23:08:26 +0200

    MAJ

---
+++ e_commerce/src/Form/CommandeType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commande;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommandeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('numero_commande')
+            ->add('date_commande')
+            ->add('nom')
+            ->add('prenom')
+            ->add('adresse')
+            ->add('cp')
+            ->add('ville')
+            ->add('Livre')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commande::class,
+        ]);
+    }
+}
