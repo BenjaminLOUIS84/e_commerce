8aa0324fb37063584aadd7049c30111c7de7ffca
Benjamin Louis <louisbenetval@orange.fr>
2023-08-19 23:44:22 +0200

    MAJ

--- e_commerce/src/Entity/Commande.php
+++ e_commerce/src/Entity/Commande.php
@@ -2,12 +2,13 @@
 
 namespace App\Entity;
 
-use App\Repository\CommandeRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-
+use App\Entity\Livre;
+use App\Entity\Facture;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CommandeRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: CommandeRepository::class)]
 class Commande
