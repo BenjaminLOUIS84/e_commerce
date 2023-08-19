6c73bd70abc4f5dfa9c21e3dfe5524544bdfacb4
Benjamin Louis <louisbenetval@orange.fr>
2023-08-19 23:21:45 +0200

    MAJ

--- e_commerce/src/Controller/CommandeController.php
+++ e_commerce/src/Controller/CommandeController.php
@@ -4,9 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Livre;
 use App\Entity\Commande;
-use Symfony\Component\HttpFoundation\Request;
+use App\Form\CommandeType;
 use App\Repository\CommandeRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
--- e_commerce/src/Form/CommandeType.php
+++ e_commerce/src/Form/CommandeType.php
@@ -2,25 +2,46 @@
 
 namespace App\Form;
 
+use App\Entity\Livre;
 use App\Entity\Commande;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CommandeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('numero_commande')
-            ->add('date_commande')
-            ->add('nom')
-            ->add('prenom')
-            ->add('adresse')
-            ->add('cp')
-            ->add('ville')
-            ->add('Livre')
+            // ->add('numero_commande')
+
+            ->add('date_commande', DateType::class, [   // Ajouter après class ['widget' => 'single_text', 'attr' =>['class' =>'form-control']] Propiété BootStrap pour améliorer l'affichage de la date
+                'label' => 'Date de commande',
+                'widget' =>'single_text',
+                'attr' =>['class' =>'form-control']
+            ])
+
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('adresse', TextType::class)
+            ->add('cp', TextType::class)
+            ->add('ville', TextType::class)
+
+            ->add('Livre', EntityType::class, [         // Particularité ici EntityType à besoin d'un tableau d'arguments pour fonctionner
+                'label' => 'Choisir un livre',
+                'class' => Livre::class, 
+                'attr' => ['class' => 'form-control'],
+                'choice_label' => 'titre'
+            ])
+
+            ->add('valider', SubmitType::class, [       // Ajouter directement le bouton submit ici
+            'attr' =>['class' => 'btn btn-dark']])      // Ajouter après class ['attr' =>['class' =>'btn btn-dark']] Pour améliorer le bouton   
         ;
+        
     }
 
     public function configureOptions(OptionsResolver $resolver): void
