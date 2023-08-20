d0a36f4765ae267aed0029b1649f68077fa261a8
Benjamin Louis <louisbenetval@orange.fr>
2023-08-20 20:18:34 +0200

    MAJ

--- e_commerce/src/Controller/CommandeController.php
+++ e_commerce/src/Controller/CommandeController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class CommandeController extends AbstractController
+ class CommandeController extends AbstractController
 {
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // FONCTION ADMINISTRATEUR POUR AFFICHER TOUTES LES COMMANDES
