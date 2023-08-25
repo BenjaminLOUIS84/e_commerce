db678cd8e5c352fb560e1da44b3700a885e0f59a
Benjamin Louis <louisbenetval@orange.fr>
2023-08-25 13:51:49 +0200

    MAJ

--- e_commerce/src/Controller/LivreController.php
+++ e_commerce/src/Controller/LivreController.php
@@ -21,18 +21,13 @@ class LivreController extends AbstractController
 {                                                                       // AFFICHER LA LISTE DES LIVRES
     #[Route('/livre', name: 'app_livre')]                               // Route représentant l'URL '/livre' pour la redirection et le name: sert pour la navigation
     
-    public function index(LivreRepository $livreRepository              // Pour afficher la liste des livres insérer dans la fonction index() livreRepository $livreRepository et Importer la classe LivreRepository avec un click droit
-                                               
-    ): Response          
-    
+    public function index(LivreRepository $livreRepository): Response   // Pour afficher la liste des livres insérer dans la fonction index() livreRepository $livreRepository et Importer la classe LivreRepository avec un click droit
     {                                                                                                                       
         $livres = $livreRepository->findBy([],["date_publication" =>    // Pour récupérer la liste des livres classées par date de publication ordre croissant
         "ASC"]);      
            
         return $this->render('livre/index.html.twig', [                 // render() Permet de faire le lien entre le controller et la view
-
             'livres' => $livres,                                        // Pour passer la variable $livres en argument 'livres'
-
         ]);
     }                                                                   // Pour afficher cet argument dans la vue il faut créer un echo représenté par {{ }} Dans le fichier index.html.twig du dossier livre
 
@@ -42,7 +37,6 @@ class LivreController extends AbstractController
     #[Route('/livre/{id}/delete', name: 'delete_livre')]                // Reprendre la route en ajoutant /{id}/delete' à l'URL et en changeant le nom du name
 
     public function delete(Livre $livre, EntityManagerInterface $entityManager): Response   
-
     {                                                                   // Créer une fonction delete() dans le controller pour supprimer un livre            
         $entityManager->remove($livre);                                 // Supprime un livre
         $entityManager->flush();                                        // Exécute l'action DANS LA BDD
@@ -120,19 +114,6 @@ class LivreController extends AbstractController
         return $this->render('livre/new.html.twig', [          // Pour faire le lien entre le controller et la vue new.html.twig (il faut donc la créer dans le dossier livre)
             'form' => $form,
             'edit' => $livre->getId()
-        ]);
-    }
-
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // FONCTION POUR AFFICHER LE DETAIL DE CHAQUE LIVRES
-
-    #[Route('/livre/{id}', name: 'show_livre')]                 // Reprendre la route en ajoutant /{id} à l'URL et en changeant le nom du name
-
-    public function show(Livre $livre): Response                // Créer une fonction show() dans le controller pour afficher le détail d'un livre 
-
-    {
-        return $this->render('livre/show.html.twig', [          // Pour faire le lien entre le controller et la vue show.html.twig (il faut donc la créer dans le dossier livre)
-            'livre' => $livre,
         ]);
     }
 
