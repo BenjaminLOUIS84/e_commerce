e4452d6fae55396fc871941fb25570d9f71efc1a
Benjamin Louis <louisbenetval@orange.fr>
2023-08-30 19:48:41 +0200

    MAJ

--- e_commerce/src/Form/ResetPasswordType.php
+++ e_commerce/src/Form/ResetPasswordType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
