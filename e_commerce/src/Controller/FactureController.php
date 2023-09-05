63e0e63bf5cb14f326ee2a5bb07d74f7b7a18760
Benjamin Louis <louisbenetval@orange.fr>
2023-09-05 17:50:58 +0200

    MAJ

--- e_commerce/src/Controller/FactureController.php
+++ e_commerce/src/Controller/FactureController.php
@@ -2,26 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Facture;
+use App\Repository\CommandeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/facture', name: 'app_facture')]
 class FactureController extends AbstractController
 {
-   
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // FONCTION POUR METTRE LA COMMANDE EN FACTURE
+    // FONCTION POUR AFFICHER LES COMMANDES DE CHAQUE FACTURES
 
-    #[Route('/ajout', name: 'add')]
-    public function add(): Response
+    #[Route('/facture/{id}', name: 'app_facture')]
+
+    public function index(Facture $facture, CommandeRepository $commandeRepository): Response
+    
     {
-        $commande = $session->get('commande', []);
-        dd($commande); 
-
+        $commandes = $commandeRepository->findBy([], ["nom" => "ASC"]);
 
         return $this->render('facture/index.html.twig', [
-            'controller_name' => 'FactureController',
+            'facture' => $facture,
+            'commandes' => $commandes
         ]);
     }
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // FONCTION POUR METTRE LA COMMANDE EN FACTURE
+
+    // #[Route('/ajout', name: 'add')]
+    // public function add(): Response
+    // {
+    //     $commande = $session->get('commande', []);
+    //     dd($commande); 
