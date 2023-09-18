5374d1085a832db0f81eaca718335f0051b4a1c6
Benjamin Louis <louisbenetval@orange.fr>
2023-09-18 22:36:14 +0200

    MAJ

--- e_commerce/src/Controller/FactureController.php
+++ e_commerce/src/Controller/FactureController.php
@@ -6,14 +6,19 @@ use App\Entity\Facture;
 use App\Service\GenPdf;
 use App\Entity\Commande;
 use App\Entity\CommandeLivre;
+use App\Service\SendMailService;
+use App\Repository\UserRepository;
 use App\Repository\FactureRepository;
+use App\Form\ResetPasswordRequestType;
 use App\Repository\CommandeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\CommandeLivreRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 #[Route('/facture', name: 'app_facture_')]  
 class FactureController extends AbstractController
@@ -112,11 +117,70 @@ class FactureController extends AbstractController
         ]);
     }
 
+    // Fonction pour afficher une notification pour prévenir le client que sa commande sera bientôt disponible
+    // et envoyer un mail pour avertir le client que l'édition numérique est disponible en téléchargement
+
     #[Route('/', name: 'notif')]
-    public function notif(): Response
+    public function notif(
+        Request $request,
+        UserRepository $userRepository,
+        // TokenGeneratorInterface $tokenGenerator,
+        EntityManagerInterface $entityManager,
+
+        //Créer un service pour envoyer des mail ou utiliser mailHog
+        SendMailService $mail
+
+    ): Response
+
     {
-        return $this->render('facture/notif.html.twig', [
-            'controller_name' => 'factureController',
-        ]);
+        $form = $this->createForm(ResetPasswordRequestType::class);         // Récupérer le formulaire
+        
+        $form->handleRequest($request);                                     // Pour traiter le formulaire
+
+        if($form->isSubmitted() && $form->isvalid()){                       // Pour vérifier si le formulaire est valide et soumis
+            
+            // On va chercher l'utilisateur par son email
+            $user = $userRepository->findOneByEmail($form->get('email')->getData());    // Pour chercher les données dans l'email qui est inscrit dans le formulaire
+        
+            // On vérifie si on un utilisateur
+            if($user){
+
+                // On génère un token de réinitialisation
+                //$token = $tokenGenerator->generateToken();
+                //$user->setResetToken($token);
+
+                //$entityManager->persist($user);                             // Pour gérer le traitement en BDD
+                //$entityManager->flush();                                    
+            
+                // On génère un lien de réinitialisation du mot de passe
+                //$url = $this->generateUrl('reset_pass', ['token' => $token],
+                //UrlGeneratorInterface::ABSOLUTE_URL);                       // Permet de générer l'url pour utiliser la nouvelle route pour créer un nouveau mot de passe
+
+                // On créer les données du mail
+                //$context = compact('url', 'user');
+
+                // Envoi du mail (Utiliser le service mail)
+                $mail->send(
+                    'etrefouetsage@gmail.com',                                  // Emetteur
+                    $user->getEmail(),                                          // Destinataire
+                    'Notification',                                             // Titre
+                    'Newsletter',                                               // Template 
+                  //  $context
+                );
+
+                $this->addFlash('success', 'Email envoyé avec succès');
+                return $this->redirectToRoute('app_login');                 // Redirection vers la page de connexion
+
+            }
+            // Cas où $user est NULL
+            $this->addFlash('danger', 'Un problème est survenu');           // En cas d'erreur on est redirigé vers la page de connexion et le message s'affichera dans cette page (*)
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('facture/notif.html.twig', [ // Passer le formulaire en arguement dans un tableau
+            
+            'requestPassForm' => $form->createView()                        // Demande pour créer le formulaire 'requestPassFrom' et pour afficher selui-ci dans une vue
+        ]);  
     }
+
 }
