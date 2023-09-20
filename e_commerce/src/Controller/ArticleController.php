d7ba37017f0d71487e1a503df85e4fc137c78e48
Benjamin Louis <louisbenetval@orange.fr>
2023-09-21 01:07:20 +0200

    MAJ

--- e_commerce/src/Controller/ArticleController.php
+++ e_commerce/src/Controller/ArticleController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController  // Afficher la liste de tous les articles classés par date ASC
 {
     #[Route('/article', name: 'app_article')]
-    public function index(): Response
+
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findBy([], ["id" => "ASC"]);
+
         return $this->render('article/index.html.twig', [
             'controller_name' => 'ArticleController',
+            'articles' => $articles
         ]);
     }
 }
