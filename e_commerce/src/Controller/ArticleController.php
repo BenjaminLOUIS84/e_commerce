a7195ef89e64874faa7401ee980763a9c94813cb
Benjamin Louis <louisbenetval@orange.fr>
2023-09-21 01:27:55 +0200

    MAJ

--- e_commerce/src/Controller/ArticleController.php
+++ e_commerce/src/Controller/ArticleController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,4 +22,24 @@ class ArticleController extends AbstractController  // Afficher la liste de tous
             'articles' => $articles
         ]);
     }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // FONCTION POUR SUPPRIMER UN ARTICLE
+
+    #[Route('/article/{id}/delete', name: 'delete_article')]                // Reprendre la route en ajoutant /{id}/delete' à l'URL et en changeant le nom du name
+
+    public function delete(Article $article, EntityManagerInterface $entityManager): Response   
+    {                                                                   // Créer une fonction delete() dans le controller pour supprimer un article            
+        $entityManager->remove($article);                                 // Supprime un article
+        $entityManager->flush();                                        // Exécute l'action DANS LA BDD
+
+        $this->addFlash(                                                // Envoyer une notification
+            'success',
+            'Supprimé avec succès!'
+        );
+
+        return $this->redirectToRoute('app_article');                     // Rediriger vers la liste des articles
+       
+    }
+
 }
