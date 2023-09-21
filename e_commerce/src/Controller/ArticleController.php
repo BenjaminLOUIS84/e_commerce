17449f02200870ed1f85c1df4cd99cda227b93f3
Benjamin Louis <louisbenetval@orange.fr>
2023-09-21 02:22:03 +0200

    MAJ

--- e_commerce/src/Controller/ArticleController.php
+++ e_commerce/src/Controller/ArticleController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
