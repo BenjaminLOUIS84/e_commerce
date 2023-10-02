25f015e585ecfc27c0a3133e3de8068e7c39027f
Benjamin Louis <louisbenetval@orange.fr>
2023-10-02 17:41:05 +0200

    MAJ

--- e_commerce/src/Controller/CommentaireController.php
+++ e_commerce/src/Controller/CommentaireController.php
@@ -2,17 +2,88 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\CommentaireRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/commentaire', name: 'app_commentaire_')]
 class CommentaireController extends AbstractController
 {
-    #[Route('/commentaire', name: 'app_commentaire')]
-    public function index(): Response
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // FONCTION pour afficher les commentaires dans un article
+
+    #[Route('/list', name: 'list')]
+    public function list(CommentaireRepository $commentaireRepository): Response
     {
-        return $this->render('commentaire/index.html.twig', [
+        $commentaires = $commentaireRepository->findBy([], ["date_com" => "DESC"]);
+
+        return $this->render('commentaire/list.html.twig', [
             'controller_name' => 'CommentaireController',
+            'commentaires' => $commentaires
         ]);
     }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // FONCTION pour préparer et ajouter les commentaires
+
+    #[Route('/prepare', name: 'prepare')]
+    #[Route('/prepare/{id}/edit', name: 'edit')]
+    public function prepare(
+        Commantaire $commantaire = null,
+        Request $request,
+        EntityManagerInterface $entityManager, 
+
+    ): Response
+
+    {
+        if(!$commentaire){
+            $commentaire = new commentaire();                               // Créer un commentaire s'il n'y en a pas
+        }
+                                        
+
+        $form = $this->createForm(CommentaireType :: class, $commentaire);  // Créer le formulaire
+        $form->handleRequest($request);                                     // Activer le formulaire
+
+        if ($form->isSubmitted() && $form->isValid()) {                     // Si le formulaire soumis est valide alors
+            
+            $commentaire = $form->getData();                                // Récupérer les informations 
+            
+            // Prepare PDO
+            $entityManager->persist($commentaire);                          // Dire à Doctrine que je veux sauvegarder la nouveau commentaire          
+            // Execute PDO
+            $entityManager->flush();                                        // Mettre le nouveau commentaire dans la BDD
+        
+            $this->addFlash('message', 'Commentaire ajouté avec succès');
+            return $this->redirectToRoute('app_commentaire_list');
+        }
+
+        return $this->render('commentaire/prepare.html.twig', [
+            'form' => $form->createView(),
+            'edit' => $commentaire->getId()
+        ]);
+        
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // FONCTION pour supprimer les commentaires
+
+    #[Route('/commentaire/{id}/delete', name: 'delete_commentaire')]                    // Reprendre la route en ajoutant /{id}/delete' à l'URL et en changeant le nom du name
+
+    public function delete(Commentaire $commentaire, EntityManagerInterface $entityManager): Response   
+    {                                                                                   // Créer une fonction delete() dans le controller pour supprimer un commentaire            
+        $entityManager->remove($commentaire);                                           // Supprime une commentaire
+        $entityManager->flush();                                                        // Exécute l'action DANS LA BDD
+
+        $this->addFlash(                                                                // Envoyer une notification
+            'success',
+            'Supprimé avec succès!'
+        );
+
+        return $this->redirectToRoute('app_commentaire_list');                               // Rediriger vers la liste des newsletterss
+    }
+
 }
--- e_commerce/src/Form/CommentaireType.php
+++ e_commerce/src/Form/CommentaireType.php
@@ -6,16 +6,26 @@ use App\Entity\Commentaire;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('commentaire')
-            ->add('dateCom')
-            ->add('user')
-            ->add('newsletters')
+            ->add('commentaire', TextareaType::class , [            // Champs pour les textes long, zone de texte pour les résumés
+                'label' => 'Contenu',
+                'attr' => ['class' => 'tinymce'],
+                'required' => false                                 // Pour rendre le résumé non obligatoire, rendre nullable la propriété resume dans la BDD
+            ])
+            // ->add('dateCom')
+            // ->add('user')
+            // ->add('newsletters')
+            ->add('Envoyer', SubmitType::class, [                   // Ajouter directement le bouton submit ici
+                'attr' =>['class' => 'btn btn-dark']
+            ])                                                      // Ajouter après class ['attr' =>['class' =>'btn btn-dark']] Pour améliorer le bouton   
+                
         ;
     }
 
