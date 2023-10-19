9b218fd59a202fbfc9a7615b08d21d19099d67e8
Benjamin Louis <louisbenetval@orange.fr>
2023-10-19 09:43:35 +0200

    MAJ

--- e_commerce/src/Controller/CommentaireController.php
+++ e_commerce/src/Controller/CommentaireController.php
@@ -55,15 +55,47 @@ class CommentaireController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {                     // Si le formulaire soumis est valide alors
 
-            $commentaire = $form->getData();                                // Récupérer les informations du fomulaire
+            // On vérifie si le champ "recaptcha-response" contient une valeur/////////CAPTCHA
+            if(empty($_POST['recaptcha-response'])){
+                header('Location: app_commentaire_prepare, id: newsletters.id'); 
+
+            }else{  // On prépare l'URL
+                $url = "https://www.google.com/recaptcha/api/siteverify?secret=6LemV_MnAAAAAMVu3oth8lvd3LVLOXoH7FMdKuJt&response={$_POST['recaptcha-response']}";
+
+                // On vérifie si CURL est installé
+                if(function_exists('curl_version')){
+                    $curl = curl_init($url);
+                    curl_setopt($curl, CURLOPT_HEADER, false);
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($curl, CURLOPT_TIMEOUT, 1);
+                    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+                    $response = curl_exec($curl);
+                }else{
+                    $response = file_get_contents($url);
+                }
+
+                // On vérifie si on a une réponse
+                if(empty($response) || is_null($response)){
+                    header('Location: app_commentaire_prepare, id: newsletters.id'); 
+                }else{
+                    $data = json_decode($response);
+                    if($data->success){
+
+                        $commentaire = $form->getData();                                // Récupérer les informations du fomulaire
             
-            // Prepare PDO
-            $entityManager->persist($commentaire);                          // Dire à Doctrine que je veux sauvegarder la nouveau commentaire          
-            // Execute PDO
-            $entityManager->flush();                                        // Mettre le nouveau commentaire dans la BDD
-        
-            $this->addFlash('message', 'Commentaire ajouté avec succès');
-            return $this->redirectToRoute('app_newsletters_list');
+                        // Prepare PDO
+                        $entityManager->persist($commentaire);                          // Dire à Doctrine que je veux sauvegarder la nouveau commentaire          
+                        // Execute PDO
+                        $entityManager->flush();                                        // Mettre le nouveau commentaire dans la BDD
+                    
+                        $this->addFlash('message', 'Commentaire ajouté avec succès');
+                        return $this->redirectToRoute('app_newsletters_list');
+
+                    }else{
+                        header('Location: app_commentaire_prepare, id: newsletters.id'); 
+                    }
+                }   
+            }  
         }
 
         return $this->render('commentaire/prepare.html.twig', [
