d3bd22bb3699de122e9d8ab2b851b655c2d7d8f4
Benjamin Louis <louisbenetval@orange.fr>
2023-10-23 14:16:47 +0200

    MAJ

--- e_commerce/src/Controller/ParamController.php
+++ e_commerce/src/Controller/ParamController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Newsletters\Users;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ParamController extends AbstractController
 {
     #[Route('/param', name: 'app_param')]
-    public function index(): Response
+    public function index(
+        // Users $users
+        ): Response
     {
         return $this->render('param/index.html.twig', [
             'controller_name' => 'ParamController',
+            // 'users' => $users
         ]);
     }
 }
