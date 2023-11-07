a470f9736c7ebb7ad97e56330163e981ff89bd4f
Benjamin Louis <louisbenetval@orange.fr>
2023-11-07 13:59:19 +0100

    MAJ

--- e_commerce/src/Controller/ParamController.php
+++ e_commerce/src/Controller/ParamController.php
@@ -7,13 +7,28 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/param', name: 'app_param_')]
 class ParamController extends AbstractController
 {
-    #[Route('/param', name: 'app_param')]
+    #[Route('/param', name: 'param')]
     public function index(): Response
     {
         return $this->render('param/index.html.twig', [
             'controller_name' => 'ParamController'
         ]);
+    }
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // // FONCTION POUR AFFICHER LA LISTE DE TOUS LES UTILISATEURS
+
+    #[Route('/', name: 'liste')]
+    public function liste(UserRepository $userRepository)
+    { 
+        if (!$this->isGranted('ROLE_ADMIN')) {                              // Permet d'empécher l'accès à cette action si ce n'est pas un admin
+            throw $this->createAccessDeniedException('Accès non autorisé');
+        }
+        
+        $users = $userRepository->findBy([], ["Pseudo" => "ASC"]);      // Affiche tous les utilisateurs
+
+        return $this->render('param/liste.html.twig', compact('users'));
     }
 }
